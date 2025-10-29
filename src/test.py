3544c831f5b5ba1048c45743b822cd075fe03e8d
Kim10903 <kimberlpena@gmail.com>
2025-10-29 12:27:13 -0600

    add workflows

---
+++ src/test.py
@@ -0,0 +1,10 @@
+# test.py
+import main
+
+def test_main_execution():
+    # Esto simplemente llama a la función main
+    main.main()
+
+# Llamada directa si quieres ejecutar el test
+if __name__ == "__main__":
+    test_main_execution()
